d50b756b8ccb9fe1b2b834a7c92d789c4909744b
Zsombor Szommer <zsombor.szommer@protonmail.com>
2022-04-15 16:54:18 +0200

    minikube tunnel

--- start.sh
+++ start.sh
@@ -5,3 +5,7 @@ kubectl create secret generic taschenka-secrets --from-literal=mongodb-password=
 kubectl apply -f ./kubernetes/taschenka_api.yaml
 
 kubectl apply -f ./kubernetes/taschenka_database.yaml
+
+echo Run 'kubectl get services', to get the EXTERNAL-IP
+
+minikube tunnel

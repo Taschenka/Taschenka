5c6ad1906e0b3486aef6045e2b8ebd23b5d8948c
Zsombor Szommer <zsombor.szommer@protonmail.com>
2022-04-15 17:06:07 +0200

    Everything is in Kubernetes
    But the EXTERNAL-IP always changes :(

--- purge.sh
+++ purge.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
 kubectl delete secret taschenka-secrets
-kubectl delete deployment taschenka-api-deployment
-kubectl delete service taschenka-api-service
+
 kubectl delete statefulset taschenka-database-statefulset
 kubectl delete service taschenka-database-service
+
+kubectl delete deployment taschenka-api-deployment
+kubectl delete service taschenka-api-service
+
+kubectl delete deployment taschenka-web-deployment
+kubectl delete service taschenka-web-service
--- start.sh
+++ start.sh
@@ -2,9 +2,11 @@
 
 kubectl create secret generic taschenka-secrets --from-literal=mongodb-password='mongoadminpassword'
 
+kubectl apply -f ./kubernetes/taschenka_database.yaml
+
 kubectl apply -f ./kubernetes/taschenka_api.yaml
 
-kubectl apply -f ./kubernetes/taschenka_database.yaml
+kubectl apply -f ./kubernetes/taschenka_web.yaml
 
 echo Run 'kubectl get services', to get the EXTERNAL-IP
 

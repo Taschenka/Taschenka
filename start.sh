3572ecf75761f81741b83f572166270d232fff13
Zsombor Szommer <zsombor.szommer@protonmail.com>
2022-04-16 19:05:21 +0200

    Ingress installed from Helm works with port-forward.
    But 'kubectl get ingress' still does not show address.

--- start.sh
+++ start.sh
@@ -8,9 +8,11 @@ kubectl apply -f ./kubernetes/taschenka_api.yaml
 
 kubectl apply -f ./kubernetes/taschenka_web.yaml
 
-helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace --set controller.setAsDefaultIngress=true
 
-kubectl wait --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
+kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
+
+kubectl annotate ingressclass nginx "ingressclass.kubernetes.io/is-default-class=true"
 
 kubectl apply -f ./kubernetes/taschenka_ingress.yaml
 
